9a0c2bab59b2eec6ea802a2b88acd42d57407360
Riteshbansal18 <riteshbansal5252@gmail.com>
2025-02-20 22:38:41 +0530

    hlo assigment 2

---
+++ server.js
@@ -0,0 +1,11 @@
+const http=require("http");
+// console.log(http);
+const server=http.createServer((req,res)=>{
+    // console.log(req);
+    res.end("g24 batch acha h");
+});
+
+
+server.listen(3000,()=>{
+    console.log("server is listening");
+});
